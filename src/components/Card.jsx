999962ac21658578c37342a1ee0eb2110709fca2
Hardik <singhhardik6014@gmail.com>
2023-05-25 09:20:45 +0530

    card mobile fixed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ function Card({ image, heading, paragraph }) {
   return (
     <div className='w-[90%] bg-white border border-secondary border-opacity-30 shadow-sm rounded my-2 text-secondary flex flex-col justify-center items-center p-4'>
         <img src={image} alt="Image" className='w-full rounded object-cover' />
-        <div className='flex flex-col justify-start items-start'>
+        <div className='flex flex-col justify-start items-start mt-4'>
             <h1 className='font-bold text-lg'>{heading}</h1>
             <p className='opacity-80 text-base'>{paragraph}</p>
             <button className='rounded-md my-2 text-secondary bg-transparent border border-secondary border-opacity-50 h-14 w-full text-base font-bold hover:bg-secondary hover:text-white transition-all mt-4'>View Campground</button>
